b155131d9aeafdc8af6612c85fbbdc974234ae6b
Namo18-web-sudo <nebula.foundation.official@gmail.com>
2025-08-01 15:15:37 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+input = input("Enter the temperature in •Celsius: ")
+if input >="20" and input <= "30":
+    print("wear medium clothes")
+elif input > "30": 
+    print("wear light clothes")
+elif input < "20":
+    print("wear heavy clothes")
+else:
+    print("Input is not a valid temperature.")
+    #hi
